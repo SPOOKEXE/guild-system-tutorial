eb7bb135acb201e32e3f383cb4370f3d49e89079
SPOOKEXE <91944990+SPOOKEXE@users.noreply.github.com>
2024-04-18 14:15:13 +0930

    correction-remove-userids

--- python/guilds.py
+++ python/guilds.py
@@ -1,12 +1,12 @@
 
 from __future__ import annotations
-import time
 from typing import Any, Union
 from fastapi import Body
 from pydantic import BaseModel
 from database import DatabaseAPI
 
 import asyncio
+import time
 
 DEFAULT_GUILD_CHAT_MESSAGE_LIMIT : int = 50
 DEFAULT_GUILD_AUDIT_LOG_LIMIT : int = 50
@@ -29,44 +29,43 @@ class InternalGuildsAPI:
 		]
 		await DatabaseAPI.register_database(InternalGuildsAPI.DATABASE_NAME, constructors)
 
-	async def IsRankPermissionsValid( permissions : dict ) -> bool:
+	async def IsRankPermissionsValid(
+			permissions : dict
+		) -> bool:
 		raise NotImplementedError
 
-	async def IsGuildNameAvailable( name : str ) -> bool:
-		query = 'SELECT guild_id FROM master WHERE name=:name COLLATE NOCASE'
-		value : Union[dict, None] = await DatabaseAPI.fetch_one(InternalGuildsAPI.DATABASE_NAME, query, {'name' : name})
-		return value is None
+	async def IsGuildNameAvailable(
+			name : str
+		) -> bool:
+		raise NotImplementedError
 
-	async def GetGuildInfoFromGuildId( guild_id : int ) -> Union[dict, None]:
-		query = 'SELECT * FROM master WHERE guild_id=:guild_id'
-		return await DatabaseAPI.fetch_one(InternalGuildsAPI.DATABASE_NAME, query, {'guild_id' : guild_id})
+	async def GetGuildInfoFromGuildId(
+			guild_id : int
+		) -> Union[dict, None]:
+		raise NotImplementedError
 
-	async def GetGuildInfoFromUserId( user_id : int ) -> Union[dict, None]:
-		query = 'SELECT guild_id FROM members WHERE user_id=:user_id'
-		value : Union[dict, None] = await DatabaseAPI.fetch_one(InternalGuildsAPI.DATABASE_NAME, query, {'user_id' : user_id})
-		if value is None: return None
-		return await InternalGuildsAPI.GetGuildInfoFromGuildId( value['guild_id'] )
+	async def GetGuildInfoFromUserId(
+			user_id : int
+		) -> Union[dict, None]:
+		raise NotImplementedError
 
-	async def GetGuildMembers( guild_id : int ) -> Union[list[dict], None]:
-		query = 'SELECT * FROM members WHERE guild_id=:guild_id'
-		return await DatabaseAPI.fetch_all(InternalGuildsAPI.DATABASE_NAME, query, {'guild_id' : guild_id})
+	async def GetGuildMembers(
+			guild_id : int
+		) -> Union[list[dict], None]:
+		raise NotImplementedError
 
-	async def GetFullGuildInfoFromGuildId( guild_id : int ) -> Union[dict, None]:
-		data = await InternalGuildsAPI.GetGuildInfoFromGuildId(guild_id)
-		if data is None: return None
-		data['members'] = await InternalGuildsAPI.GetGuildMembers( guild_id )
-		# data['banned'] = await InternalGuildsAPI.GetGuildBannedUserIds( guild_id, user_id )
-		return data
+	async def GetFullGuildInfoFromGuildId(
+			guild_id : int
+		) -> Union[dict, None]:
+		raise NotImplementedError
 
-	async def GetFullGuildInfoFromUserId( user_id : int ) -> Union[dict, None]:
-		query = 'SELECT guild_id FROM members WHERE user_id=:user_id'
-		value : Union[dict, None] = await DatabaseAPI.fetch_one(InternalGuildsAPI.DATABASE_NAME, query, {'user_id' : user_id})
-		if value is None: return None
-		return await InternalGuildsAPI.GetFullGuildInfoFromGuildId( value['guild_id'] )
+	async def GetFullGuildInfoFromUserId(
+			user_id : int
+		) -> Union[dict, None]:
+		raise NotImplementedError
 
 	async def UpdateGuildDisplayInfo(
 		guild_id : int,
-		user_id : int,
 		description : str,
 		accessibility : int,
 		emblem : int
@@ -79,50 +78,10 @@ class InternalGuildsAPI:
 		description : str,
 		emblem : int
 	) -> Union[dict, None]:
-
-		info = await InternalGuildsAPI.GetGuildInfoFromUserId(user_id)
-		if info is not None: return None
-
-		is_name_available : bool = await InternalGuildsAPI.IsGuildNameAvailable(name)
-		if is_name_available is False: return None
-
-		# insert to master and get the data
-		query : str = 'INSERT INTO master (name, description, emblem, owner_id, creation) VALUES (:name, :description, :emblem, :owner_id, :creation);'
-		await DatabaseAPI.execute_one(InternalGuildsAPI.DATABASE_NAME, query, {
-			'name' : name, 'description' : description, 'emblem' : emblem, 'owner_id' : user_id, 'creation' : get_time()
-		})
-
-		query : str = 'SELECT * FROM master DESC LIMIT 1'
-		guild_data : Union[dict, None] = await DatabaseAPI.fetch_one(InternalGuildsAPI.DATABASE_NAME, query, None)
-		if guild_data is None: return None
-
-		# insert default ranks and get their ids
-		query : str = 'INSERT INTO ranks (guild_id, name, permissions) VALUES (:guild_id, :name, :permissions);'
-		await DatabaseAPI.execute_one(InternalGuildsAPI.DATABASE_NAME, query, {'guild_id' : guild_data['guild_id'], 'name' : 'Owner', 'permissions' : '{}'})
-		await DatabaseAPI.execute_one(InternalGuildsAPI.DATABASE_NAME, query, {'guild_id' : guild_data['guild_id'], 'name' : 'Member', 'permissions' : '{}'})
-
-		query : str = 'SELECT rank_id FROM ranks WHERE guild_id=:guild_id '
-		rank_ids : Union[list[dict], None] = await DatabaseAPI.fetch_all(InternalGuildsAPI.DATABASE_NAME, query, {'guild_id' : guild_data['guild_id']})
-		rank_ids : list[int] = [ item['rank_id'] for item in rank_ids ]
-		owner_rank_id : int = rank_ids[0]
-		member_rank_id : int = rank_ids[1]
-
-		# insert to members
-		query : str = 'INSERT INTO members (guild_id, user_id, rank_id, timestamp) VALUES (:guild_id, :user_id, :rank_id, :timestamp);'
-		await DatabaseAPI.execute_one(InternalGuildsAPI.DATABASE_NAME, query, {
-			'guild_id' : guild_data['guild_id'], 'user_id' : user_id, 'rank_id' : owner_rank_id, 'timestamp' : get_time()
-		})
-
-		# update guild info
-		query : str = 'UPDATE master SET default_rank_id=:default_rank_id WHERE guild_id=:guild_id'
-		await DatabaseAPI.execute_one(InternalGuildsAPI.DATABASE_NAME, query, { 'guild_id' : guild_data['guild_id'], 'default_rank_id' : member_rank_id })
-
-		# return all information
-		return await InternalGuildsAPI.GetFullGuildInfoFromGuildId( guild_data['guild_id'] )
+		raise NotImplementedError
 
 	async def ChangeUserIdRankInGuild(
 		guild_id : int,
-		user_id : int,
 		target_id : int,
 		rank_id : int
 	) -> None:
@@ -130,7 +89,6 @@ class InternalGuildsAPI:
 
 	async def ChangeRankPermissionsInGuild(
 		guild_id : int,
-		user_id : int,
 		rank_id : int,
 		permissions : dict
 	) -> None:
@@ -138,46 +96,72 @@ class InternalGuildsAPI:
 
 	async def CreateRankInGuild(
 		guild_id : int,
-		user_id : int,
 		name : str,
 		protected : bool = Body(False, embed=True)
 	) -> None:
-		# TODO: add 'protected' ranks that cannot be deleted (DefaultRank & Owner Rank)
 		raise NotImplementedError
 
-	async def RemoveRankInGuild( guild_id : int, user_id : int, rank_id : int ) -> None:
+	async def RemoveRankInGuild(
+			guild_id : int,
+			rank_id : int
+		) -> None:
 		raise NotImplementedError
 
-	async def SetDefaultRankInGuild( guild_id : int, user_id : int, rank_id : int ) -> None:
+	async def SetDefaultRankInGuild(
+			guild_id : int,
+			rank_id : int
+		) -> None:
 		raise NotImplementedError
 
-	async def KickUserIdFromGuild( guild_id : int, user_id : int, target_id : int ) -> None:
+	async def KickUserIdFromGuild(
+			guild_id : int,
+			target_id : int
+		) -> None:
 		raise NotImplementedError
 
-	async def DeleteGuild( guild_id : int, user_id : int ) -> None:
+	async def DeleteGuild(
+			guild_id : int
+		) -> None:
 		raise NotImplementedError
 
-	async def TransferGuildOwnership( guild_id : int, user_id : int, target_id : int ) -> None:
+	async def TransferGuildOwnership(
+			guild_id : int,
+			target_id : int
+		) -> None:
 		raise NotImplementedError
 
-	async def GetGuildBannedUserIds( guild_id : int, user_id : int ) -> None:
+	async def GetGuildBannedUserIds(
+			guild_id : int
+		) -> None:
 		raise NotImplementedError
 
-	async def BanUserIdFromGuild( guild_id : int, user_id : int, target_id : int ) -> None:
+	async def BanUserIdFromGuild(
+			guild_id : int,
+			target_id : int
+		) -> None:
 		raise NotImplementedError
 
-	async def UnbanUserIdFromGuild( guild_id : int, user_id : int, target_id : int ) -> None:
+	async def UnbanUserIdFromGuild(
+			guild_id : int,
+			target_id : int
+		) -> None:
 		raise NotImplementedError
 
-	async def CreateGuildChatMessage( guild_id : int, user_id : int, message : str ) -> None:
+	async def CreateGuildChatMessage(
+			guild_id : int,
+			user_id : int,
+			message : str
+		) -> None:
 		raise NotImplementedError
 
-	async def RemoveGuildChatMessage( guild_id : int, user_id : int, message_id : int ) -> None:
+	async def RemoveGuildChatMessage(
+			guild_id : int,
+			message_id : int
+		) -> None:
 		raise NotImplementedError
 
 	async def GetGuildChatMessages(
 		guild_id : int,
-		user_id : int,
 		offset : int = Body(0, embed=True),
 		limit : int = Body(DEFAULT_GUILD_CHAT_MESSAGE_LIMIT, embed=True)
 	) -> None:
@@ -193,7 +177,6 @@ class InternalGuildsAPI:
 
 	async def GetGuildAuditLogs(
 		guild_id : int,
-		user_id : int,
 		offset : int = Body(0, embed=True),
 		limit : int = Body(DEFAULT_GUILD_AUDIT_LOG_LIMIT, embed=True)
 	) -> None:
--- python/webpoints.py
+++ python/webpoints.py
@@ -39,12 +39,11 @@ async def GetGuildInfoFromUserId(
 @guilds_api.post('/update-guild-display-info', summary='UpdateGuildDisplayInfo', tags=['guild-core'])
 async def UpdateGuildDisplayInfo(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True),
 	description : str = Body(None, embed=True),
 	accessibility : int = Body(None, embed=True),
 	emblem : int = Body(None, embed=True)
 ) -> Union[dict, None]:
-	return await InternalGuildsAPI.UpdateGuildDisplayInfo(guild_id, user_id, description, accessibility, emblem)
+	return await InternalGuildsAPI.UpdateGuildDisplayInfo(guild_id, description, accessibility, emblem)
 
 @guilds_api.post('/create-guild', summary='CreateGuild', tags=['guild-core'])
 async def CreateGuild(
@@ -53,123 +52,85 @@ async def CreateGuild(
 	description : str = Body(None, embed=True),
 	emblem : int = Body(None, embed=True)
 ) -> Union[dict, None]:
-	return await InternalGuildsAPI.GetGuildInvites(user_id, name, description, emblem)
-
-@guilds_api.post('/get-guild-invites', summary='GetGuildInvites', tags=['guild-core'])
-async def GetGuildInvites(
-	user_id : int = Body(None, embed=True),
-) -> None:
-	return await InternalGuildsAPI.GetGuildInvites(user_id)
-
-@guilds_api.post('/invite-user-id-to-guild', summary='InviteUserIdToGuild', tags=['guild-core'])
-async def InviteUserIdToGuild(
-	user_id : int = Body(None, embed=True),
-	target_id : int = Body(None, embed=True),
-) -> None:
-	return await InternalGuildsAPI.InviteUserIdToGuild(user_id, target_id)
-
-@guilds_api.post('/accept-guild-invite', summary='AcceptGuildInvite', tags=['guild-core'])
-async def AcceptGuildInvite(
-	user_id : int = Body(None, embed=True),
-	invite_id : int = Body(None, embed=True),
-) -> None:
-	return await InternalGuildsAPI.AcceptGuildInvite(user_id, invite_id)
+	return await InternalGuildsAPI.CreateGuild(user_id, name, description, emblem)
 
 @guilds_api.post('/change-user-id-rank-in-guild', summary='ChangeUserIdRankInGuild', tags=['guild-core'])
 async def ChangeUserIdRankInGuild(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True),
 	target_id : int = Body(None, embed=True),
 	rank_id : int = Body(None, embed=True)
 ) -> None:
-	return await InternalGuildsAPI.ChangeUserIdRankInGuild(guild_id, user_id, target_id, rank_id)
+	return await InternalGuildsAPI.ChangeUserIdRankInGuild(guild_id, target_id, rank_id)
 
 @guilds_api.post('/change-guild-rank-permissions', summary='ChangeGuildRankPermissions', tags=['guild-core'])
 async def ChangeRankPermissionsInGuild(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True),
 	rank_id : int = Body(None, embed=True),
 	permissions : dict = Body(None, embed=True)
 ) -> None:
-	return await InternalGuildsAPI.ChangeRankPermissionsInGuild(guild_id, user_id, rank_id, permissions)
+	return await InternalGuildsAPI.ChangeRankPermissionsInGuild(guild_id, rank_id, permissions)
 
 @guilds_api.post('/create-rank-in-guild', summary='CreateRankInGuild', tags=['guild-core'])
 async def CreateRankInGuild(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True),
 	name : str = Body(None, embed=True),
 	protected : bool = Body(False, embed=True)
 ) -> None:
-	return await InternalGuildsAPI.CreateRankInGuild(guild_id, user_id, name, protected=protected)
+	return await InternalGuildsAPI.CreateRankInGuild(guild_id, name, protected=protected)
 
 @guilds_api.post('/remove-rank-in-guild', summary='RemoveRankInGuild', tags=['guild-core'])
 async def RemoveRankInGuild(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True),
 	rank_id : int = Body(None, embed=True)
 ) -> None:
-	return await InternalGuildsAPI.IgnoreGuildInvite(guild_id, user_id, rank_id)
+	return await InternalGuildsAPI.RemoveRankInGuild(guild_id, rank_id)
 
 @guilds_api.post('/set-default-rank-in-guild', summary='SetDefaultRankInGuild', tags=['guild-core'])
 async def SetDefaultRankInGuild(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True),
 	rank_id : int = Body(None, embed=True)
 ) -> None:
-	return await InternalGuildsAPI.IgnoreGuildInvite(guild_id, user_id, rank_id)
+	return await InternalGuildsAPI.SetDefaultRankInGuild(guild_id, rank_id)
 
 @guilds_api.post('/kick-user-id-from-guild', summary='KickUserIdFromGuild', tags=['guild-core'])
 async def KickUserIdFromGuild(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True),
 	target_id : int = Body(None, embed=True),
 ) -> None:
-	return await InternalGuildsAPI.IgnoreGuildInvite(guild_id, user_id, target_id)
-
-@guilds_api.post('/ignore-guild-invite', summary='IgnoreGuildInvite', tags=['guild-core'])
-async def IgnoreGuildInvite(
-	user_id : int = Body(None, embed=True),
-	invite_id : str = Body(None, embed=True)
-) -> None:
-	return await InternalGuildsAPI.IgnoreGuildInvite(user_id, invite_id)
+	return await InternalGuildsAPI.KickUserIdFromGuild(guild_id, target_id)
 
 @guilds_api.post('/delete-guild', summary='DeleteGuild', tags=['guild-core'])
 async def DeleteGuild(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True)
 ) -> None:
-	return await InternalGuildsAPI.DeleteGuild(guild_id, user_id)
+	return await InternalGuildsAPI.DeleteGuild(guild_id)
 
 @guilds_api.post('/transfer-guild-ownership', summary='TransferGuildOwnership', tags=['guild-core'])
 async def TransferGuildOwnership(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True),
 	target_id : int = Body(None, embed=True)
 ) -> None:
-	return await InternalGuildsAPI.TransferGuildOwnership(guild_id, user_id, target_id)
+	return await InternalGuildsAPI.TransferGuildOwnership(guild_id, target_id)
 
 @guilds_api.post('/get-guild-banned-user-ids', summary='GetGuildBannedUserIds', tags=['guild-core'])
 async def GetGuildBannedUserIds(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True)
 ) -> None:
-	return await InternalGuildsAPI.GetGuildBannedUserIds(guild_id, user_id)
+	return await InternalGuildsAPI.GetGuildBannedUserIds(guild_id)
 
 @guilds_api.post('/ban-user-id-from-guild', summary='BanUserIdFromGuild', tags=['guild-core'])
 async def BanUserIdFromGuild(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True),
 	target_id : int = Body(None, embed=True)
 ) -> None:
-	return await InternalGuildsAPI.BanUserIdFromGuild(guild_id, user_id, target_id)
+	return await InternalGuildsAPI.BanUserIdFromGuild(guild_id, target_id)
 
 @guilds_api.post('/unban-user-id-from-guild', summary='UnbanUserIdFromGuild', tags=['guild-core'])
 async def UnbanUserIdFromGuild(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True),
 	target_id : int = Body(None, embed=True)
 ) -> None:
-	return await InternalGuildsAPI.UnbanUserIdFromGuild(guild_id, user_id, target_id)
+	return await InternalGuildsAPI.UnbanUserIdFromGuild(guild_id, target_id)
 
 @guilds_api.post('/create-guild-chat-message', summary='CreateGuildChatMessage', tags=['guild-chat'])
 async def CreateGuildChatMessage(
@@ -182,19 +143,17 @@ async def CreateGuildChatMessage(
 @guilds_api.post('/remove-guild-chat-message', summary='RemoveGuildChatMessage', tags=['guild-chat'])
 async def RemoveGuildChatMessage(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True),
 	message_id : int = Body(None, embed=True)
 ) -> None:
-	return await InternalGuildsAPI.RemoveGuildChatMessage(guild_id, user_id, message_id)
+	return await InternalGuildsAPI.RemoveGuildChatMessage(guild_id, message_id)
 
 @guilds_api.post('/get-guild-chat-messages', summary='GetGuildChatMessages', tags=['guild-chat'])
 async def GetGuildChatMessages(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True),
 	offset : int = Body(0, embed=True),
 	limit : int = Body(DEFAULT_GUILD_CHAT_MESSAGE_LIMIT, embed=True)
 ) -> None:
-	return await InternalGuildsAPI.GetGuildChatMessages(guild_id, user_id, offset, limit)
+	return await InternalGuildsAPI.GetGuildChatMessages(guild_id, offset, limit)
 
 @guilds_api.post('/create-guild-audit-log', summary='CreateGuildAuditLog', tags=['guild-audit-log'])
 async def CreateGuildAuditLog(
@@ -208,11 +167,10 @@ async def CreateGuildAuditLog(
 @guilds_api.post('/get-guild-audit-logs', summary='GetGuildAuditLogs', tags=['guild-audit-log'])
 async def GetGuildAuditLogs(
 	guild_id : int = Body(None, embed=True),
-	user_id : int = Body(None, embed=True),
 	offset : int = Body(0, embed=True),
 	limit : int = Body(DEFAULT_GUILD_AUDIT_LOG_LIMIT, embed=True)
 ) -> None:
-	return await InternalGuildsAPI.GetGuildAuditLogs(guild_id, user_id, offset, limit)
+	return await InternalGuildsAPI.GetGuildAuditLogs(guild_id, offset, limit)
 
 @guilds_api.post('/get-full-guild-info-from-guild-id', summary='GetFullGuildInfoFromGuildId', tags=['guild-core'])
 async def GetFullGuildInfoFromGuildId( guild_id : int, user_id : int ) -> Union[dict, None]:
